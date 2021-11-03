2f2bae5416fa605f27d52294d9f34d3ace5e0ac9
Alexander Mann <amann@c2r1p10.hive.fi>
2021-11-03 17:22:34 +0200

    adding ft_strcpy and ft_strncpy

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:41:30 by amann             #+#    #+#             */
-/*   Updated: 2021/11/03 16:27:10 by amann            ###   ########.fr       */
+/*   Updated: 2021/11/03 17:17:01 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@
 
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
-size_t	ft_strlen(char *str);
 void	ft_putnbr(int n);
-int		ft_strcmp(char *s1, char *s2);
-char	*ft_strdup(char *src);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -32,6 +29,12 @@ void	*ft_memccpy(void *dest, void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+size_t	ft_strlen(char *str);
+char	*ft_strdup(char *src);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+int		ft_strcmp(char *s1, char *s2);
 
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
