e09e2962ed7fc0cb9f716fd5d6e18fd9e23314d8
Alexander Mann <amann@c1r5p13.hive.fi>
2021-11-08 18:14:17 +0200

    ft_strtrim, done

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:41:30 by amann             #+#    #+#             */
-/*   Updated: 2021/11/08 17:04:59 by amann            ###   ########.fr       */
+/*   Updated: 2021/11/08 18:11:13 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,5 +69,6 @@ int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
 
 #endif
---
+++ srcs/ft_strtrim.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 17:24:22 by amann             #+#    #+#             */
+/*   Updated: 2021/11/08 18:10:51 by amann            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	size_t	i;
+	char	*res;
+
+	start = 0;
+	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
+		start++;
+	end = ft_strlen(s) - 1;
+	while (s[end] == ' ' || s[end] == '\n' || s[end] == '\t')
+		end--;
+	len = end - start + 1;
+	res = (char *) malloc((len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		res[i] = s[start + i]; 
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
