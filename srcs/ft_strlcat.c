f17c6b50c93b9bedd9c098e6cafbddfe85af8d3a
Alexander Mann <amann@c1r5p11.hive.fi>
2021-11-15 12:01:15 +0200

    ft_strlcat now passes all tests

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,11 +6,14 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 10:49:54 by amann             #+#    #+#             */
-/*   Updated: 2021/11/04 16:16:24 by amann            ###   ########.fr       */
+/*   Updated: 2021/11/15 11:54:42 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// #include <string.h>
+// #include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -18,14 +21,43 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	len_src;
 	size_t	i;
 
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
+	len_dst = strlen(dst);
+	len_src = strlen(src);
+	if (len_dst > dstsize)
+		return (len_src + dstsize);
 	i = 0;
-	while (src[i] != '\0' && len_dst + i < dstsize - 1)
+	while (src[i] != '\0' && (dstsize - len_dst - 1 - i) > 0)
 	{
 		dst[len_dst + i] = src[i];
 		i++;
 	}
+	// if (dst[i] == '\0')
+	// 	return (len_src);
+	// else if (src[i] != '\0')
+	// 	return (len_src + dstsize);
 	dst[len_dst + i] = '\0';
 	return (len_dst + len_src);
 }
+
+// int	main(void)
+// {
+// 	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+// 	char buff1[0xF00] = "there is no stars in the sky"; 
+// 	char buff2[0xF00] = "there is no stars in the sky";
+// 	size_t max = strlen(str) + 4;
+// 	size_t r1 = strlcat(buff1, str, max);
+// 	size_t r2 = ft_strlcat(buff2, str, max);	
+// 	printf("max = %zu dst_size = %zu\n", max, strlen("there is no stars in the sky"));
+// 	printf("strlcat: %zu || ft_strlcat: %zu\n", r1, r2);
+// 	printf("strlcat: %s \nft_strlcat: %s\n", buff1, buff2);
+
+// 	char s1[4] = "";
+// 	char s2[4] = "";
+// 	printf("%lu\n", strlen("thx to ntoniolo for this test !"));
+
+// 	size_t r3 = strlcat(s1, "thx to ntoniolo for this test !", 4);
+// 	size_t r4 = ft_strlcat(s2, "thx to ntoniolo for this test !", 4);
+// 	printf("strlcat: %zu || ft_strlcat: %zu\n", r3, r4);
+// 	printf("strlcat: %s || ft_strlcat: %s\n", s1, s2);
+// 	return (0);
+// }
