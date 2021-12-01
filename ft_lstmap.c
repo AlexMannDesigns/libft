a0c7e12adc50cfd0be1796a0c06650f125057fe2
Alexander Mann <amann@c1r4p5.hive.fi>
2021-12-01 12:30:42 +0200

    maybe the finall commit before evalls, lstmap done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:39:46 by amann             #+#    #+#             */
-/*   Updated: 2021/11/22 18:15:02 by amann            ###   ########.fr       */
+/*   Updated: 2021/12/01 12:25:29 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list *mapped;
-	t_list *next_map;
+	t_list *tmp;
 
 	if (!lst || !f)
 		return (NULL);
-	mapped = f(lst);
-	if (lst->next)
+	if (lst)
 	{
-		next_map = ft_lstmap(lst->next, f);
-		if (!next_map)
-		{
-			free(mapped);
-			return (NULL);
-		}
-		lst->next = next_map;
+		tmp = f(lst);
+		tmp->next = ft_lstmap(lst->next, f);
+		return (tmp);
 	}
-	return (mapped);
+	return (NULL);
 }
