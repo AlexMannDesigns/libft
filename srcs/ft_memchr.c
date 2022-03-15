c12111082f250429a6c56273fe5ac1b8a932249f
Alexander Mann <amann@c1r4p7.hive.fi>
2022-03-15 13:43:50 +0200

    overhaul of libft structure, added ft_printf, added get next line

---
+++ srcs/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 15:09:07 by amann             #+#    #+#             */
+/*   Updated: 2021/11/12 15:27:14 by amann            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s + i) == (unsigned char)c)
+			return ((unsigned char *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
