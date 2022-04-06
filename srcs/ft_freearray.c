b809e97200f246fda61294c245568930e59f30b6
Alexander Mann <amann@c1r4p7.hive.fi>
2022-04-06 16:32:35 +0300

    removing wildcards from Makefile

--- srcs/ft_freearray.c
+++ srcs/ft_freearray.c
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 16:43:03 by amann             #+#    #+#             */
-/*   Updated: 2022/03/18 17:24:53 by amann            ###   ########.fr       */
+/*   Updated: 2022/04/06 15:50:12 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_freearray(void ***arr, size_t size)
 	i = 0;
 	while (i <= size)
 	{
-		if (*((*arr + i))
+		if (*((*arr) + i))
 		{	
 			free(*((*arr) + i));
 			*((*arr) + i) = NULL;
